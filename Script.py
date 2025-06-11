124d3a504bb4b8235290d1258ad9e7853edc0caa
unniiimon <unniiimon123@gmail.com>
2025-06-11 08:19:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,7 +246,10 @@ Nᴀᴍᴇ - {}"""
 
     CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
-<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
+<b>⚙️ sɪᴢᴇ : {file_size}</b>"""
+
+ ✳️ ᴊᴏɪɴ :- @moviecenter225
+ ✳️ ᴊᴏɪɴ :- @mcserieshub
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
