67954c1295f3b40932d8e4e1657e46ff5884bfe1
unniiimon <unniiimon123@gmail.com>
2025-06-15 09:43:43 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,8 +4,6 @@
 
 from aiohttp import web
 from .route import routes
-from keep_alive import keep_alive
-keep_alive()
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
